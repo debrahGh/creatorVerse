f0995f6697633cd8faa15933b0c6d5c225c02fc7
debrahGh <111481786+debrahGh@users.noreply.github.com>
2024-08-30 19:36:08 -0500

    Update CreatorCard.js

--- src/components/CreatorCard.js
+++ src/components/CreatorCard.js
@@ -1,1 +1,15 @@
+import React from 'react';
+
+const CreatorCard = ({ creator }) => {
+  return (
+    <div className="creator-card">
+      <h3>{creator.name}</h3>
+      <p>{creator.description}</p>
+      {creator.imageURL && <img src={creator.imageURL} alt={creator.name} />}
+      <a href={creator.url} target="_blank" rel="noopener noreferrer">Visit Channel</a>
+    </div>
+  );
+};
+
+export default CreatorCard;
 

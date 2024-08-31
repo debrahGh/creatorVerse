6d16002112d2023eef47034a71c72f50985982a7
debrahGh <111481786+debrahGh@users.noreply.github.com>
2024-08-30 20:39:34 -0500

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'your-supabase-url';
-const supabaseKey = 'your-supabase-key';
+const supabaseUrl = 'https://crvsbrzikbkhvwibojxf.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNydnNicnppa2JraHZ3aWJvanhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ2NDA4MDMsImV4cCI6MjA0MDIxNjgwM30.pZPHM2-7NBzvtyjTqnXRKMuB-SDfz5tPjuSFTVEecRY';
 export const supabase = createClient(supabaseUrl, supabaseKey);

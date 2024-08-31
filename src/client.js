069e2a81d46fd94dcde36f88a55f363697c555e5
debrahGh <111481786+debrahGh@users.noreply.github.com>
2024-08-30 20:07:35 -0500

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const URL = 'your-supabase-project-url';
-const API_KEY = 'your-supabase-api-key';
-
-export const supabase = createClient(URL, API_KEY);
+const supabaseUrl = 'your-supabase-url';
+const supabaseKey = 'your-supabase-key';
+export const supabase = createClient(supabaseUrl, supabaseKey);
